4b877efe7f042c1f405556f063b677f2c9e44841
Fraida Fund <ffund@nyu.edu>
2019-01-23 12:44:48 -0500

    Updated script for parsing wil6210 to include timestamp, accept path as argument

--- parse-bf.sh
+++ parse-bf.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-BFPATH=sample-bf.txt
+BFPATH=$1
 
+ts=$(date +%s.%N)
 bf=$(cat $BFPATH)
 sector=$(echo "$bf"| grep "Sectors"  | cut -d":" -f3 | awk '{print $1}')
 sqi=$(echo "$bf" | grep "SQI"  | cut -d"=" -f2 | awk '{print $1}')
@@ -10,4 +11,4 @@ txmcs=$(echo "$bf" | grep "TxMCS"  | cut -d"=" -f2 | awk '{print $1}')
 txtput=$(echo "$bf" | grep "TxTpt"  | cut -d"=" -f3 | awk '{print $1}')
 goodrx=$(echo "$bf" | grep "Goodput" | cut -d":" -f2 | awk '{print $2}')
 goodtx=$(echo "$bf" | grep "Goodput" | cut -d":" -f3 | awk '{print $1}')
-echo "$sector,$sqi,$rssi,$txmcs,$txtput,$goodrx,$goodtx"
+echo "$ts,$sector,$sqi,$rssi,$txmcs,$txtput,$goodrx,$goodtx"
